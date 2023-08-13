f45f2815206345e71782cadbfe3e3e4e367df13f
Siyanne <sirayanne.ket@gmail.com>
2023-08-14 01:57:17 +0200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Accueil from "./Components/Accueil/Accueil.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+        </Routes>
+      </div>
+      <nav>
+        <a href="index.html">Accueil</a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          A propos
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      </nav>
+      <h1>Chez vous, partout et ailleurs</h1>
+      <template>
+        <div className="card">
+          <a href="#">
+            <img src="" alt="" />
+            <p>Edit</p>
+          </a>
+        </div>
+      </template>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Accueil/Accueil.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Accueil() {
+  <div>
+    <h1>Chez vous, partout et ailleurs</h1>
+  </div>;
+}
